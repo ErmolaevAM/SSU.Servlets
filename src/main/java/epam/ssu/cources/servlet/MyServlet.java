8b010a7e669875361f2ddc9ea7b49367ca8706d8
ErmolaevAM <chepucha@yandex.ru>
2018-02-18 22:02:54 +0400

    change absolute path for file to universal path

--- src/main/java/epam/ssu/cources/servlet/MyServlet.java
+++ src/main/java/epam/ssu/cources/servlet/MyServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -15,12 +16,14 @@ import java.util.List;
 
 public class MyServlet extends HttpServlet {
 
+    private ClassLoader classLoader = getClass().getClassLoader();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         RequestDispatcher dispatcher = req.getRequestDispatcher("items.jsp");
         List<Item> items = new ArrayList<Item>();
         String line = null;
-        try (BufferedReader reader = new BufferedReader(new FileReader("D:\\ermolaxe\\IdeaProjects\\ermolaxe.mock\\WebApp-Servlets\\src\\main\\resources\\items.txt"))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(new File(classLoader.getResource("items.txt").getFile())))) {
             while ((line = reader.readLine()) != null) {
                 String[] words = line.split(":");
                 items.add(new Item(Integer.parseInt(words[0]), words[1], Integer.parseInt(words[2]), words[3]));
